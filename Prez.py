6881014956faedbdb54fd5958432404c5bfcff9c
NathisaMx <142730533+NathisaMx@users.noreply.github.com>
2024-02-11 18:30:30 +0100

    Update Prez.py

--- Prez.py
+++ Prez.py
@@ -7,7 +7,7 @@ df=pd.read_csv(r"C:\Users\isanm\Projet Professionnel\Formations\Data Analyste\Wi
 st.title("Analyse des feux de forêts aux USA")
 st.sidebar.title("Feux de forêts aux USA")
 st.sidebar.header("Déroulé du projet")
-pages=["Présentation du jeu de données", "Préparation des données", "Data Visualisation", "Modélisation","Time Series"]
+pages=["Présentation du jeu de données", "Préparation des données", "Data Visualisation", "Modélisation", "Time Series"]
 page=st.sidebar.radio("Allez vers", pages)
 if page == pages[0] :
     st.write("## Présentation du jeu de données : première exploration")
