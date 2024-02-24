1ee31d938abceab324876062276e6dc62e410251
NathisaMx <142730533+NathisaMx@users.noreply.github.com>
2024-02-24 10:46:09 +0100

    Update 6_📊 _Data_Visualisation.py

--- pages/6_📊 _Data_Visualisation.py
+++ pages/6_📊 _Data_Visualisation.py
@@ -4,52 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
-
 import gdown
 
 # Replace 'FILE_ID' with the actual ID of your Google Drive file
 file_id = '1m73Ot3M2O2YPPH1urzuTejOaOEQuDi2P'
-
 # Google Drive API endpoint to download the file
 url = f'https://drive.google.com/uc?id={file_id}'
-
-
-
 file = gdown.download(url,output="wildfire_base_clean.zip")
-
 # Convert the downloaded file bytes to a BytesIO object
 # file_in_memory = BytesIO(file_bytes)
 
 df=pd.read_csv(file,compression='zip')
 st.set_page_config(page_title="Data Visualisation", page_icon="Fire_logo.png",)
-
-
 pages=["Data Viz' générale","Data Viz' régionale","Data Viz' temporelle"]
 
 page = st.sidebar.radio("Cliquez sur la partie à afficher", pages)
-
-
 if page == pages[0] :
     tab1, tab2, tab3 = st.tabs(["Taille des feux","Dichotomie des feux", "Focus sur les Mégafeux"])
     with tab1:
         st.header("Focus sur la variable Taille des feux")
         st.dataframe(df.taille_feu.describe())
-   
-
-if page == pages[1] :
-    st.write("## Préparation des données : complétude des données")
-    st.write('###Titre')
-    st.write('blablabla')
-    st.divider()
-
-
-
-
-
-
-
-
-        
+     
     with tab2:
         col1, col2, col3 = st.columns([1,3,1])
         col1.write("")
