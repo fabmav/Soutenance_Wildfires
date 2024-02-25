3679ed98012102fbe4fb4723cbaf9006432cef19
NathisaMx <142730533+NathisaMx@users.noreply.github.com>
2024-02-25 20:18:55 +0100

    Dispersion (#20)

---
+++ Texte/Dispersions.md
@@ -0,0 +1,11 @@
+
+
+
+Nous observons une très grande disparité de la taille des feux :
+* sur l'ensemble du jeu de données, la dispersion est énorme avec un écart-type qui approche 900 Ha, des feux minuscules et :orange[**75% des feux font moins de 1.21 Ha**]
+* Hors Mégafeux, la taille moyenne est divisée par 2 passant de 25.7 à 13.47 Ha ; l'écart-type est de 185 Ha
+* les Mégafeux ne représentent qu'une partie infime des feux, :orange[**952 feux sur presque 2,25 millions**] : dans le dernier quartile la taille passe de :orange[**moins de 30 000 Ha à 268 185 Ha**]
+
+Nous verrons que cette :orange[**grande hétérogénéité des feux**] n'est pas sans conséquence pour la suite de l'analyse ni pour la complexité des prédictions.
+
+Ces observations nous ont conduit à analyser les feux de notre jeu de données sous deux angles :orange[**le nombre de feux**] et :orange[**la surface d'hectares brûlés**]
--- pages/6_📊 _Data_Visualisation.py
+++ pages/6_📊 _Data_Visualisation.py
@@ -6,51 +6,87 @@ import seaborn as sns
 import pickle
 import gdown
 
-# Replace 'FILE_ID' with the actual ID of your Google Drive file
-file_id = '1m73Ot3M2O2YPPH1urzuTejOaOEQuDi2P'
-# Google Drive API endpoint to download the file
-url = f'https://drive.google.com/uc?id={file_id}'
-file = gdown.download(url,output="wildfire_base_clean.zip")
-# Convert the downloaded file bytes to a BytesIO object
-# file_in_memory = BytesIO(file_bytes)
+with open("stat_méga.pkl", "rb") as fichier_pickle:
+    description_feux = pickle.load(fichier_pickle)
+    description_Mégafeux = pickle.load(fichier_pickle)
+    description_Hors_Méga = pickle.load(fichier_pickle)
+    Surf_Total = pickle.load(fichier_pickle)
+    Surf_méga= pickle.load(fichier_pickle)
+    Part_méga_NB = pickle.load(fichier_pickle)
+    Part_méga_surf= pickle.load(fichier_pickle)
 
-df=pd.read_csv(file,compression='zip')
+with open("Texte/Dispersions.md", "r", encoding="utf-8") as fichier_markdown:
+    Com_Dispersions = fichier_markdown.read()
+
 st.set_page_config(page_title="Data Visualisation", page_icon="Fire_logo.png",)
 pages=["Data Viz' générale","Data Viz' régionale","Data Viz' temporelle"]
 
 page = st.sidebar.radio("Cliquez sur la partie à afficher", pages)
-if page == pages[0] :
+
+if page == pages[0]:
     tab1, tab2, tab3 = st.tabs(["Taille des feux","Dichotomie des feux", "Focus sur les Mégafeux"])
+
     with tab1:
-        st.header("Focus sur la variable Taille des feux")
-        st.dataframe(df.taille_feu.describe())
-     
-    with tab2:
-        col1, col2, col3 = st.columns([1,3,1])
+        
+        # Centrer le titre
+        st.markdown("<h2 style='text-align: center;background: linear-gradient(to right, orange, red); -webkit-background-clip: text; color: transparent;'>Dispersion statistique comparative des feux selon leur taille (Mégafeux est >= 10 000 Ha)</h2>", unsafe_allow_html=True)
+        col1, col2, col3, col4 = st.columns([2,6,2,2])
+        col1.markdown("<h4 style='text-align: center;'>Ensemble des feux</h4>", unsafe_allow_html=True)
+        col2.markdown("<h4 style='text-align: center; background: linear-gradient(to right, navy, orange, red); -webkit-background-clip: text; color: transparent;'>Principales observations</h4>", unsafe_allow_html=True)
+        col3.markdown("<h4 style='text-align: center;'>Hors Mégafeux</h4>", unsafe_allow_html=True)
+        col4.markdown("<h4 style='text-align: center;'>Mégafeux</h4>", unsafe_allow_html=True)
+        col1, col2, col3,col4 = st.columns([2,6,2,2])
+        col1.table(description_feux)
+        col2.markdown(Com_Dispersions, unsafe_allow_html=True)
+        col3.table(description_Hors_Méga)
+        col4.table(description_Mégafeux)
+        col1, col2, col3 = st.columns([2,8,2])
         col1.write("")
-        col2.write("## La dichotomie des feux")
+        col2.image("Graphes/Boxplot_Taille.png", width=1000)
         col3.write("")
-        st.write("Nous pouvons observer dans le jeu de données, 2 « tendances », illustrées par une série de 2 graphiques sur la même variable")
+        
+
+
+    with tab2:
+        st.markdown("<h2 style='text-align: center; background: linear-gradient(to right, orange, red); -webkit-background-clip: text; color: transparent;'>La dichotomie des feux</h2>", unsafe_allow_html=True)
+        st.markdown("<h4 style='text-align: center;'>Les observations précédentes nous ont conduit à faire une analyse des variables en fonction du nombre de feux et de la surface brûlée</h4>", unsafe_allow_html=True)
         col1, col2 = st.columns(2)
-        col1.write("Les feux nombreux, de petite taille,  de cause humaine")
-        col2.write("Les feux plus rares, de taille importante, de cause naturelle")
         st.divider()
-        st.image("Variable_Cause.png",width=1100)
+        col1.markdown("<h3 style='text-align: center; background: linear-gradient(to right, orange, red); -webkit-background-clip: text; color: transparent;'>Les feux nombreux, de petite taille,  de cause humaine</h3>", unsafe_allow_html=True)
+        col2.markdown("<h3 style='text-align: center; background: linear-gradient(to right, orange, red); -webkit-background-clip: text; color: transparent;'>Les feux plus rares, de taille importante, de cause naturelle</h3>", unsafe_allow_html=True)
+        col1, col2, col3 = st.columns([1,4,1])
+        col1.write("")
+        col2.image("Graphes/Variable_Classe.png",width=1200)
+        col3.write("")
         st.divider()
-        st.image("Variable_Classe.png",width=1100)
+        col1, col2, col3 = st.columns([1,4,1])
+        col1.write("")
+        col2.image("Graphes/Variable_Cause.png",width=1200)
+        col3.write("")
         st.divider()
-        st.image("Variable_mois.png", width=1100)
+        col1, col2, col3 = st.columns([1,4,1])
+        col1.write("")
+        col2.image("Graphes/Variable_mois.png",width=1200)
+        col3.write("")
         st.divider()
-        st.image("Variable_année.png", width=1100)
+        col1, col2, col3 = st.columns([1,4,1])
+        col1.write("")
+        col2.image("Graphes/Variable_region.png",width=1200)
+        col3.write("")
         st.divider()
-        st.image("Variable_region.png", width=1100)
-        st.divider()
-        st.image("Variable_vegetation.png", width=1100)
+        col1, col2, col3 = st.columns([1,4,1])
+        col1.write("")
+        col2.image("Graphes/Variable_vegetation.png",width=1200)
+     
+
+
+
 
     with tab3:
         st.header("Focus sur les mégafeux")
 
 if page == pages[1] :
+
     st.write("###Relation entre nombre de feux et données géographiques")
     st.write("##Les régions Southeast et Southwest représentent le plus grand nombres de feux entre 1992 et 2020.") 
     st.divider()
@@ -80,5 +116,6 @@ if page == pages[2] :
     st.write("## Temporelle")
     st.write('###Titre')
     st.write('blablabla')
+    st.image("Variable_année.png", width=1100)
     st.divider()
 
